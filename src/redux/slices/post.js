9a2a916f0c3e187360b457cf9bf1322a354df631
GreatCode1989 <wef666wef6@gmail.com>
2023-10-13 19:16:36 +0300

    lo

--- src/redux/slices/post.js
+++ src/redux/slices/post.js
@@ -1,20 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const { data } = await axios.get("/posts");
+  return data;
+});
+
+export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
+    const { data } = await axios.get("/tags");
+    return data;
+  });
+  
 
 const initialState = {
-    posts: {
-        items: [],
-        status: "loading"
-    },
-    tags: {
-        items: [],
-        status: "loading"
-    }
-}
+  posts: {
+    items: [],
+    status: "loading",
+  },
+  tags: {
+    items: [],
+    status: "loading",
+  },
+};
 
 const postsSlice = createSlice({
-    name: "posts",
-    initialState,
-    reduser: {}
-})
+  name: "posts",
+  initialState,
+  reduсer: {},
+  extraReducers: {
+    [fetchPosts.pending]: (state) => {
+        state.posts.items = []
+      state.posts.status = "loading";
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+        state.posts.items = action.payload
+        state.posts.status = "loaded";
+      },
+      [fetchPosts.rejected]: (state, action) => {
+        state.posts.items = []
+        state.posts.status = "error";
+      },
+      [fetchTags.pending]: (state) => {
+        state.tags.items = []
+      state.tags.status = "loading";
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+        state.tags.items = action.payload
+        state.tags.status = "loaded";
+      },
+      [fetchTags.rejected]: (state, action) => {
+        state.tags.items = []
+        state.tags.status = "error";
+      },
+  },
+});
 
-export const postsReducer = postsSlice.reducer
+export const postsReducer = postsSlice.reducer;
